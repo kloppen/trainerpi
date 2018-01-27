a9c02565ae219c76de6dd7d1f9a94330f6093d4f
Stefan Kloppenborg <klopps@gmail.com>
2018-01-27 00:29:55 +0000

    non-working bluepy example

--- connect.py
+++ connect.py
@@ -1,18 +1,42 @@
-import pygatt
+from bluepy import btle
+# from btle import Peripheral, ADDR_TYPE_RANDOM, AssignedNumbers
 from binascii import hexlify
+import time
 
-adapter = pygatt.GATTToolBackend()
+class CSC(btle.Peripheral):
+    def __init__(self, addr):
+        btle.Peripheral.__init__(self, addr, addrType=btle.ADDR_TYPE_RANDOM)
 
-def handle_data(handle, value):
-    print("Received data: {}".format(hexlify(value)))
+if __name__=="__main__":
+    cccid = btle.AssignedNumbers.client_characteristic_configuration
+    cscid = btle.AssignedNumbers.cyclingSpeedAndCadence
+    cscmid = btle.AssignedNumbers.csc_measurement
 
-try:
-    adapter.start()
-    device = adapter.connect("D0:AC:A5:BF:B7:52", timeout=20)
-    
-    device.subscribe("00002a5b-0000-1000-8000-00805f9b34fb",
-            callback=handle_data)
+    csc = None
+    try:
+        csc = CSC("D0:AC:A5:BF:B7:52")
+        service, = [s for s in csc.getServices() if s.uuid==cscid]
+        ccc, = service.getCharacteristics(forUUID=str(cscmid))
+        
+        if 0: # This doesn't work
+            ccc.write('\1\0')
+        else:
+            desc = csc.getDescriptors(service.hndStart,
+                                      service.hndEnd)
+            d = [d[0] for d in desc if d.uuid==cscid]
+            csc.writeCharacteristic(d.handle, '\1\0')
 
-finally:
-    adapter.stop()
+        t0=time.time()
+        
+        def print_csc(cHandle, data):
+            print("data: {}".format(hexlify(data)))
+
+        csc.delegate.handleNotification = print_csc
+
+        for x in range(10):
+            csc.waitForNotifications(3.)
+
+    finally:
+        if csc:
+            csc.disconnect()
 
