48573025c60c13f0a577bafe2c788570a28f8c61
Stefan Kloppenborg <klopps@gmail.com>
2018-02-09 17:42:39 -0500

    started refactoring

--- trainerpi.py
+++ trainerpi.py
@@ -1,11 +1,10 @@
 import bleCSC
 import numpy
 import threading
+# import curses
 
 
 ROLLING_LENGTH = 2096.  # mm
-
-
 power_curve = numpy.loadtxt("power-4.csv", delimiter=",")
 
 
@@ -19,18 +18,24 @@ def handle_speed_notification(wheel_speed: float, crank_speed: float) -> None:
     ))
 
 
-def wheel_thread_worker():
-    wheel_sensor = bleCSC.CSCSensor("D0:AC:A5:BF:B7:52", handle_speed_notification)
-    location_wheel = wheel_sensor.get_location()
-    print("Location (wheel_sensor): {}".format(location_wheel))
-    wheel_sensor.notifications(True)
-    while True:
-        try:
-            if wheel_sensor.wait_for_notifications(1.0):
-                continue
-            print("Waiting for wheel...")
-        except (KeyboardInterrupt, SystemExit):
-            break
+class CSCThread(threading.Thread):
+    def __init__(self, group=None, name=None):
+        threading.Thread.__init__(self, group=group, target=self.worker, name=name)
+        self.address = ""
+        self.name = ""
+
+    def worker(self):
+        sensor = bleCSC.CSCSensor(self.address, handle_speed_notification)
+        location_wheel = sensor.get_location()
+        print("Location (wheel_sensor): {}".format(location_wheel))
+        sensor.notifications(True)
+        while True:
+            try:
+                if sensor.wait_for_notifications(1.0):
+                    continue
+                print("Waiting for wheel...")
+            except (KeyboardInterrupt, SystemExit):
+                break
 
 
 def crank_thread_worker():
@@ -47,10 +52,17 @@ def crank_thread_worker():
             break
 
 
-wheel_thread = threading.Thread(target=wheel_thread_worker)
+# def main_screen(stdscr):
+#     stdscr.clear()
+
+wheel_thread = CSCThread()
+wheel_thread.address = "D0:AC:A5:BF:B7:52"
+wheel_thread.name = "Sensor 1"
+
 crank_thread = threading.Thread(target=crank_thread_worker)
 
 wheel_thread.start()
 crank_thread.start()
 
-print("Exiting")
+
+# curses.wrapper(main_screen)
