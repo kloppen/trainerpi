3fabce850bcbca383d5be13a16bf778b10badbe9
Stefan Kloppenborg <klopps@gmail.com>
2018-02-09 17:52:11 -0500

    Updated text display to use curses

--- trainerpi.py
+++ trainerpi.py
@@ -1,7 +1,7 @@
 import bleCSC
 import numpy
 import threading
-# import curses
+import curses
 
 
 ROLLING_LENGTH = 2096.  # mm
@@ -12,16 +12,18 @@ class CSCThread(threading.Thread):
     def __init__(self, group=None, name=None):
         threading.Thread.__init__(self, group=group, target=self.worker, name=name)
         self.address = ""
-        self.name = ""
+        self.number = 0
+        self.stdscr = None
 
     def handle_notification(self, wheel_speed: float, crank_speed: float) -> None:
         speed = wheel_speed * 3600. * ROLLING_LENGTH / 1e+6
         power = numpy.interp(speed, power_curve[:, 0], power_curve[:, 1])
-        print("Wheel: {:2.0f} km/h, Power: {:3.0f} W, Crank: {:3.0f}".format(
+        self.stdscr.addstr(self.number, 0, "Wheel: {:2.0f} km/h, Power: {:3.0f} W, Crank: {:3.0f}".format(
             wheel_speed * 3600. * ROLLING_LENGTH / 1e+6,
             power,
             crank_speed * 60.
         ))
+        self.stdscr.refresh()
 
     def worker(self):
         sensor = bleCSC.CSCSensor(self.address, self.handle_notification)
@@ -32,24 +34,27 @@ class CSCThread(threading.Thread):
             try:
                 if sensor.wait_for_notifications(1.0):
                     continue
-                print("Waiting for {}...".format(self.name))
+                self.stdscr.addstr(self.number, 0, "Waiting for Sensor {}...".format(self.number))
+                self.stdscr.refresh()
             except (KeyboardInterrupt, SystemExit):
                 break
 
 
-# def main_screen(stdscr):
-#     stdscr.clear()
+def main_screen(stdscr):
+    stdscr.clear()
 
-sensor1_thread = CSCThread()
-sensor1_thread.address = "D0:AC:A5:BF:B7:52"
-sensor1_thread.name = "Sensor 1"
+    sensor1_thread = CSCThread()
+    sensor1_thread.address = "D0:AC:A5:BF:B7:52"
+    sensor1_thread.number = 1
+    sensor1_thread.stdscr = stdscr
 
-sensor2_thread = CSCThread()
-sensor2_thread.address = "C6:F9:84:6A:C0:8E"
-sensor2_thread.name = "Sensor 2"
+    sensor2_thread = CSCThread()
+    sensor2_thread.address = "C6:F9:84:6A:C0:8E"
+    sensor2_thread.number = 2
+    sensor2_thread.stdscr = stdscr
 
-sensor1_thread.start()
-sensor2_thread.start()
+    sensor1_thread.start()
+    sensor2_thread.start()
 
 
-# curses.wrapper(main_screen)
+curses.wrapper(main_screen)
