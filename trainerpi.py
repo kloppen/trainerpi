b94e1fa8ca74552c634e84c4d09803615b5397e6
Stefan Kloppenborg <klopps@gmail.com>
2018-02-09 17:46:20 -0500

    More refactoring

--- trainerpi.py
+++ trainerpi.py
@@ -8,24 +8,23 @@ ROLLING_LENGTH = 2096.  # mm
 power_curve = numpy.loadtxt("power-4.csv", delimiter=",")
 
 
-def handle_speed_notification(wheel_speed: float, crank_speed: float) -> None:
-    speed = wheel_speed * 3600. * ROLLING_LENGTH / 1e+6
-    power = numpy.interp(speed, power_curve[:, 0], power_curve[:, 1])
-    print("Wheel: {:2.0f} km/h, Power: {:3.0f} W, Crank: {:3.0f}".format(
-        wheel_speed * 3600. * ROLLING_LENGTH / 1e+6,
-        power,
-        crank_speed * 60.
-    ))
-
-
 class CSCThread(threading.Thread):
     def __init__(self, group=None, name=None):
         threading.Thread.__init__(self, group=group, target=self.worker, name=name)
         self.address = ""
         self.name = ""
 
+    def handle_notification(self, wheel_speed: float, crank_speed: float) -> None:
+        speed = wheel_speed * 3600. * ROLLING_LENGTH / 1e+6
+        power = numpy.interp(speed, power_curve[:, 0], power_curve[:, 1])
+        print("Wheel: {:2.0f} km/h, Power: {:3.0f} W, Crank: {:3.0f}".format(
+            wheel_speed * 3600. * ROLLING_LENGTH / 1e+6,
+            power,
+            crank_speed * 60.
+        ))
+
     def worker(self):
-        sensor = bleCSC.CSCSensor(self.address, handle_speed_notification)
+        sensor = bleCSC.CSCSensor(self.address, self.handle_notification)
         location_wheel = sensor.get_location()
         print("Location (wheel_sensor): {}".format(location_wheel))
         sensor.notifications(True)
@@ -33,36 +32,24 @@ class CSCThread(threading.Thread):
             try:
                 if sensor.wait_for_notifications(1.0):
                     continue
-                print("Waiting for wheel...")
+                print("Waiting for {}...".format(self.name))
             except (KeyboardInterrupt, SystemExit):
                 break
-
-
-def crank_thread_worker():
-    crank_sensor = bleCSC.CSCSensor("C6:F9:84:6A:C0:8E", handle_speed_notification)
-    location_crank = crank_sensor.get_location()
-    print("Location (crank_sensor): {}".format(location_crank))
-    crank_sensor.notifications(True)
-    while True:
-        try:
-            if crank_sensor.wait_for_notifications(1.0):
-                continue
-            print("Waiting for crank...")
-        except (KeyboardInterrupt, SystemExit):
-            break
 
 
 # def main_screen(stdscr):
 #     stdscr.clear()
 
-wheel_thread = CSCThread()
-wheel_thread.address = "D0:AC:A5:BF:B7:52"
-wheel_thread.name = "Sensor 1"
+sensor1_thread = CSCThread()
+sensor1_thread.address = "D0:AC:A5:BF:B7:52"
+sensor1_thread.name = "Sensor 1"
 
-crank_thread = threading.Thread(target=crank_thread_worker)
+sensor2_thread = CSCThread()
+sensor2_thread.address = "C6:F9:84:6A:C0:8E"
+sensor2_thread.name = "Sensor 2"
 
-wheel_thread.start()
-crank_thread.start()
+sensor1_thread.start()
+sensor2_thread.start()
 
 
 # curses.wrapper(main_screen)
