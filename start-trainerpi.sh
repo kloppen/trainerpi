0026bb8375ab31bc0a00ffba331b75d469a9ee28
Stefan Kloppenborg <klopps@gmail.com>
2018-07-20 20:47:19 -0400

    Completed startup script

--- start-trainerpi.sh
+++ start-trainerpi.sh
@@ -4,5 +4,12 @@
 bluetoothctl power on
 
 # Launch the script, assuming that there is a virtualenv named trainerpi-ve
-trainerpi-ve/bin/python trainerpi.py
+# But before trying to launch, ensure that there is only one instance
+if pgrep -f "python trainerpi.py" > /dev/null
+then
+    echo "trainerpi is already running"
+else
+    cd ~/trainerpi/
+    trainerpi-ve/bin/python trainerpi.py
+fi
 
